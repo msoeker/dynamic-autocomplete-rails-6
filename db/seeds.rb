6258d7532fb996948cd14b1cc7eaeb2f5654fc0c
msoeker <80034462+msoeker@users.noreply.github.com>
2021-06-30 14:47:06 +0200

    seed db input field install jquery

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require "faker"
+
+Person.create(first_name: 'Immanuel', last_name: 'Kant')
+Person.create(first_name: 'Marie', last_name: 'Dorian')
+Person.create(first_name: 'William', last_name: 'Kunstler')
+Person.create(first_name: 'J.C.R.', last_name: 'Licklider')
+
+100.times do
+  person = Person.new(
+    first_name: Faker::Name.unique.first_name ,
+    last_name: Faker::Name.unique.last_name 
+  )
+  person.save
+end
